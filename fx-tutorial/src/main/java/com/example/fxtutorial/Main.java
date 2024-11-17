65f3bb991131e5b4dba31555e7b689f8381a5c19
Anna <ajasielec@student.agh.edu.pl>
2024-11-17 10:57:18 +0100

    scene builder setup

--- fx-tutorial/src/main/java/com/example/fxtutorial/Main.java
+++ fx-tutorial/src/main/java/com/example/fxtutorial/Main.java
@@ -1,90 +1,26 @@
 package com.example.fxtutorial;
 
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCombination;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Polygon;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
 
-import java.awt.*;
-import java.awt.image.ImageObserver;
-import java.awt.image.ImageProducer;
-
 public class Main extends Application {
-
-
-    public static void main(String[] args) {
-        launch(args);
-    }
 
     @Override
     public void start(Stage stage) throws Exception {
-        Group root = new Group();
-        Scene scene = new Scene(root,600, 600, Color.HOTPINK);
-
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         Image icon = new Image(getClass().getResourceAsStream("/cat-icon.png"));
         stage.getIcons().add(icon);
         stage.setTitle("Stage Demo Program");
-
-        Text text = new Text("Witaj");   // text - type of node
-        text.setX(50);
-        text.setY(50);
-        text.setFont(Font.font("Verdana", 50));
-        text.setFill(Color.PEACHPUFF);
-
-        Line line = new Line();
-        line.setStartX(200);
-        line.setStartY(200);
-        line.setEndX(500);
-        line.setEndY(200);
-        line.setStrokeWidth(5);
-        line.setStroke(Color.DARKRED);
-        line.setOpacity(0.5);
-        line.setRotate(45);
-
-        Rectangle rectangle = new Rectangle();
-        rectangle.setX(100);
-        rectangle.setY(100);
-        rectangle.setWidth(100);
-        rectangle.setHeight(100);
-        rectangle.setFill(Color.BLUEVIOLET);
-        rectangle.setStrokeWidth(5);
-        rectangle.setStroke(Color.BLACK);
-
-        Polygon triangle = new Polygon();
-        triangle.getPoints().setAll(200.0, 200.0, 300.0, 300.0, 200.0, 300.0);
-        triangle.setFill(Color.GHOSTWHITE);
-
-        Circle circle = new Circle();
-        circle.setCenterX(350);
-        circle.setCenterY(350);
-        circle.setRadius(50);
-        circle.setFill(Color.BLACK);
-
-        Image image = new Image(getClass().getResourceAsStream("/pupper.jpg"));
-        ImageView imageView = new ImageView(image);
-        imageView.setX(100);
-        imageView.setY(100);
-        imageView.setFitHeight(200);
-        imageView.setFitWidth(200);
-
-        root.getChildren().add(text);
-        root.getChildren().add(line);
-        root.getChildren().add(rectangle);
-        root.getChildren().add(triangle);
-        root.getChildren().add(circle);
-        root.getChildren().add(imageView);
-
-        stage.setScene(scene);
+        stage.setScene(new Scene(root));
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
