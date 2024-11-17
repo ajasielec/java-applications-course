7636ad42c3ac87fcaaf29380c05552dccaa1019e
Anna <ajasielec@student.agh.edu.pl>
2024-11-17 11:28:00 +0100

    event handling

---
+++ fx-tutorial/src/main/java/com/example/fxtutorial/Controller.java
@@ -0,0 +1,36 @@
+package com.example.fxtutorial;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+
+public class Controller {
+
+    @FXML
+    // private Circle mycircle;
+    private ImageView imageView;
+    private double x;
+    private double y;
+
+    @FXML
+    public void initialize() {
+        x = imageView.getLayoutX();
+        y = imageView.getLayoutY();
+    }
+
+    public void up(ActionEvent event) {
+        imageView.setLayoutY(y-=3);
+    }
+    public void down(ActionEvent event) {
+        imageView.setLayoutY(y+=3);
+    }
+    public void left(ActionEvent event) {
+        imageView.setLayoutX(x-=3);
+    }
+    public void right(ActionEvent event) {
+        imageView.setLayoutX(x+=3);
+    }
+}
