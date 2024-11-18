f4215e715499461c3063c023163104ba50ed230b
Anna <ajasielec@student.agh.edu.pl>
2024-11-18 18:36:09 +0100

    logout/exit button

--- fx-tutorial/src/main/java/com/example/fxtutorial/Main.java
+++ fx-tutorial/src/main/java/com/example/fxtutorial/Main.java
@@ -1,9 +1,12 @@
 package com.example.fxtutorial;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
@@ -13,15 +16,34 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         try{
-            Parent root = FXMLLoader.load(getClass().getResource("scene1.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("logout.fxml"));
             Image icon = new Image(getClass().getResourceAsStream("/cat-icon.png"));
             stage.getIcons().add(icon);
             stage.setTitle("JavaFX tutorial");
 
             stage.setScene(new Scene(root));
             stage.show();
+
+            stage.setOnCloseRequest(event -> {
+                    event.consume();
+                    logout(stage);
+                    });
+
         } catch(Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public void logout(Stage stage) {
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("You're about to logout!");
+        alert.setContentText("Do you want to save before exiting?: ");
+
+        if (alert.showAndWait().get() == ButtonType.OK) {
+            System.out.println("Logout button clicked");
+            stage.close();
         }
     }
 
