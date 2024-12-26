1a2357b5a301bbf463dc6deecc118b16c3f1ce33
Ania <ajasielec@student.agh.edu.pl>
2024-12-26 18:20:03 +0100

    Create entities

---
+++ 5-springboot/src/main/java/com/example/model/Teacher.java
@@ -0,0 +1,30 @@
+package com.example.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    private TeacherCondition teacherCondition;
+
+    private int birthYear;
+    private int salary;
+
+    @Column(nullable = true)
+    private Long groupId;
+}
---
+++ 5-springboot/src/main/java/com/example/model/TeacherCondition.java
@@ -0,0 +1,8 @@
+package com.example.model;
+
+public enum TeacherCondition {
+    PRESENT,
+    ABSENT,
+    SICK,
+    DELEGATION
+}
